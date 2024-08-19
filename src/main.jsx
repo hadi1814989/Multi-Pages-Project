bfef8263ab3b8b1b3d5ad390f180959e5c3fe50f
hadi1814989 <freelancer1814989@gmail.comgit config --global user.email freelancer1814989@gmail.com>
2024-08-19 22:41:12 +0600

    add

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
